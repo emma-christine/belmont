c578474c8de18642f024fec799ca1b4b63923943
Emma Harrington <harrington.emma.c@gmail.com>
2022-03-15 16:19:00 -0600

    added quotes page with vue

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        quotes: [{
+            text: "\"The only boy that talks to me is the Holy Ghost\"",
+            author: ""
+        }, {
+            text: "\"Your dog isn't cuter than mine because my dog doesn't exist and yours shouldn't either\"",
+            author: ""
+        }, {
+            text: "\"I don't think I could ever vibe with someone that hot, it would be weird.\"",
+            author: ""
+        }, {
+            text: "\"You don't sing in the shower? Are you just depressed all the time??\"",
+            author: ""
+        }, {
+            text: "\"I shouldn't show you my friends. One of them was an underwear model IN PERU\"",
+            author: "- Aaron"
+        }, {
+            text: "\"Are you just sitting there with your pants open like some shady man?\"",
+            author: ""
+        }, {
+            text: "\"I won't sleep when I'm dead. I'll be in the spirit world and it's going to suck!\"",
+            author: ""
+        }, {
+            text: "\"I'm a freaking missionary, do you think I have time to get laid??\"",
+            author: ""
+        }, {
+            text: "\"That is not even true. Go flush yourself down the freaking toilet\"",
+            author: ""
+        }, {
+            text: "\"You kissed him with your eyes OPEN? And you thought this was going to work out??\"",
+            author: ""
+        }, {
+            text: "\"One time I got really deep into corset youtube\"",
+            author: ""
+        }, {
+            text: "\"I went on a date with a blind guy and even HE opened like 6 doors for me\"",
+            author: ""
+        }, {
+            text: "\"There's no way in heaven or hell that I'm sharing that\"",
+            author: ""
+        }, {
+            text: "\"You are just ruining my nugget experience\"",
+            author: ""
+        },],
+        newQuote: '',
+        newQuoter: '',
+        colors: [{
+            class: "one"
+        }, {
+            class: "two"
+        }, {
+            class: "three"
+        }, {
+            class: "four"
+        }, {
+            class: "five"
+        },],
+        place: 0,
+    },
+    methods: {
+        addItem() {
+            if (this.newQuote != '') {
+                this.quotes.push({
+                    text: '"' + this.newQuote + '"',
+                    author: "- " + this.newQuoter
+                });
+            }
+            this.newQuote = '';
+            this.newQuoter = '';
+        },
+        getNextColor() {
+            color = colors[place];
+            place = place + 1;
+            if (place == 5) {
+                place = 0;
+            }
+            return color;
+        }
+    },
+});
+
 
 function validateEmail() {
     console.log("lel");
